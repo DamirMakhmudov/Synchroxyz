582e67aa301cb0f26b886c417b388c932d32f0f9
Damir <d.makhmudov@kazan.csoft.ru>
2021-12-10 14:59:02 +0300

    Add project files.

---
+++ JsonObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync365
+{
+    public class JsonObject
+    {
+        public JsonObject() { }
+        public string task { get; set; }
+        public string user { get; set; }
+        public string signature { get; set; }
+        public string outputpdf { get; set; }
+        public string guid { get; set; }
+        public string[] files { get; set; }
+        public string chatid { get; set; }
+        public string text { get; set; }
+    }
+}
---
+++ Sync365WebApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tdms.Api;
+using Tdms.Log;
+using System.IO;
+using System.Text;
+using System.Reflection;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Sync365
+{
+    public class Sync365WebApiController : ControllerBase
+    {
+        public TDMSApplication ThisApplication;
+        public ILogger Logger { get; set; }
+        public TDMSObject thisobject;
+        public JsonObject jsonobject;
+        public string response;
+
+        public Sync365WebApiController(TDMSApplication application)
+        {
+            ThisApplication = application;
+            Logger = Tdms.Log.LogManager.GetLogger("Sync365WebApi");
+        }
+        [Route("api/testpost"), HttpPost]
+        public string PostJson([FromBody] JsonObject jsonobjectO)
+        {
+            jsonobject = jsonobjectO;
+            string taskText = jsonobject.task.ToString().ToLower();
+            //var req = this.Request;
+            Logger.Info("hello!");
+            response = "cool";
+            return response;
+        }
+    }
+
+
+}
